5440db59eaec4bdb5c46e9c6fe20c75fd054ebce
Kamil Cichoń <78487952+Seqiiu@users.noreply.github.com>
2021-09-23 15:20:21 +0200

    Add new solution " PP0506A - Sort 1"
    Add new solution " PP0506A - Sort 1"

---
+++ easy/PP0506A - Sort 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PP0506A
+{
+    public class Obj : IComparable<Obj>
+    {
+        public string Name { get; set; }
+        public int OsX { get; set; }
+        public int OsY { get; set; }
+        public double Lenght { get; set; }
+        public Obj(string name, int osX, int osY)
+        {
+            Name = name;
+            OsX = osX;
+            OsY = osY;
+            Lenght = Math.Sqrt(osX * osX + osY * osY);
+        }
+        public int CompareTo(Obj other)
+        {
+            return Lenght.CompareTo(other.Lenght);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ilosc = int.Parse(Console.ReadLine());
+            for (int i = 0; i < ilosc; i++)
+            {
+                var objs = new List<Obj>();
+                int n = int.Parse(Console.ReadLine());
+                for (int j = 0; j < n; j++)
+                {
+                    string[] kSub = Console.ReadLine().Split(' ');
+                    objs.Add(new Obj(kSub[0], int.Parse(kSub[1]), int.Parse(kSub[2])));
+                }
+                objs.Sort();
+                foreach (var item in objs)
+                {
+                    Console.WriteLine($"{item.Name} {item.OsX} {item.OsY}");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
