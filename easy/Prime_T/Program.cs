21fffbaefd162275c6636d71d648f2c5016c5ed4
Seqiiu <fajo1ful@gmail.com>
2021-09-23 21:24:52 +0200

    Prime_T Solve

---
+++ easy/Prime_T/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PRIME_T
+{
+    class Program
+    {
+        private static bool CzyPierwsza(int n)
+        {
+            if (n < 2)
+                return false;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int liczba = int.Parse(Console.ReadLine());
+                if (CzyPierwsza(liczba))
+                    Console.WriteLine("TAK");
+                else
+                    Console.WriteLine("NIE");
+            }
+        }
+    }
+}
